1e677c2c831ef6ea80a4a61f1fe07bf656144a97
miklik99 <miklik.martin01@gmail.com>
2018-11-22 18:05:22 +0100

    Navbar-mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.sass';
+import Header from './Components/Header/Header';
+import Intro from './Components/Intro/Intro';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Intro />
       </div>
     );
   }
---
+++ src/Components/Intro/Intro.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Intro.sass';
+
+class Intro extends Component {
+  render() {
+    return (
+      <div className="Intro">
+
+      </div>
+    );
+  }
+}
+
+export default Intro;
