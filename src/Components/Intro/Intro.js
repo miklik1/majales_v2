dced53a77dbacfc22661493ae01f958e19893065
miklik99 <miklik.martin01@gmail.com>
2018-11-22 18:56:10 +0100

    trochu intro

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './Intro.sass';
+import boss from './boss.png';
+import cubeBG from './cubeBG.png';
 
 class Intro extends Component {
   render() {
     return (
       <div className="Intro">
-
+	      <div className="introImages">
+	      	<div className="jmenoBox"> Kapitán Demo </div>
+	      	<img className='cubeBG' src={cubeBG} alt='cubeBG' />
+	      	<img className='boss' src={boss} alt='boss' />
+	      </div>
       </div>
     );
   }
