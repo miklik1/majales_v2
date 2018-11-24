bcad70baf265256d4acbef0474a89a34bf965535
miklik99 <miklik.martin01@gmail.com>
2018-11-24 22:51:14 +0100

    dalsi sekce

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header/Header';
 import Intro from './Components/Intro/Intro';
 import Vyhod from './Components/Vyhod/Vyhod';
 import Vyber from './Components/Vyber/Vyber';
+import Nejsme from './Components/Nejsme/Nejsme';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Intro />
         <Vyhod />
         <Vyber />
+        <Nejsme />
       </div>
     );
   }
---
+++ src/Components/Nejsme/Nejsme.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Nejsme.sass';
+
+class Nejsme extends Component {
+  render() {
+    return (
+      <div className="Nejsme">
+        <p className="kopyto">Nejsme na to sami</p>
+        <p className="obyc">Každá akce si žádá své partnery. My můžeme být rádi, že máme ty nejlepší z nejlepších.
+        Podívejte se, na čem pracují oni!</p>
+      </div>
+    );
+  }
+}
+
+export default Nejsme;
