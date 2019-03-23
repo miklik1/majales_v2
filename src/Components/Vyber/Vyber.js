af5535f440b10110c9feaa7bba09c45fda801199
miklik99 <40503150+miklik99@users.noreply.github.com>
2019-03-23 14:28:37 +0100

    novy

--- src/Components/Vyber/Vyber.js
+++ src/Components/Vyber/Vyber.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './Vyber.sass';
+import guitars from './guitars.png';
 
 class Vyber extends Component {
   render() {
     return (
       <div className="Vyber">
         <div className="d-flex bd-highlight razeni">
-          <div className="p-2 flex-fill bd-highlight sekce1"><p>Sleduj nás!</p></div>
-          <div className="p-2 flex-fill bd-highlight sekce2"><p>Lístek za polovinu</p></div>
+          <div className="p-2 flex-fill bd-highlight sekce1">
+            <p>DOPROVODNÝ PROGRAM</p>
+          </div>
+          <div className="p-2 flex-fill bd-highlight sekce2">
+            <p>line up</p>
+            <img className='guitars' src={guitars} alt='guitars' />
+          </div>
         </div>
+        <div className="sekce3"><p>ROZHOVORY S OSOBNOSTMI</p></div>
       </div>
     );
   }
--- src/Components/Vyhod/Vyhod.js
+++ src/Components/Vyhod/Vyhod.js
@@ -5,9 +5,11 @@ class Vyhod extends Component {
   render() {
     return (
       <div className="Vyhod">
-        <p className="kopyto">VYHOĎ SI Z KOPÝTKA!</p>
-        <p className="obyc">Samé chcíplé zeschlé kundy. Co s tím konečníkem? Mlčení páni zkušení, rád bych vám Majáles nabídnul. Proč se to nepřegrcne tady do toho?</p>
-        <button>Koupit lístek</button>
+      	<p className="info-line">7. 5. 2019  I   Zlínský lyžařský svah   I   Vstup zdarma</p>
+        <p className="kopyto">Dojdi si vyhodit z kopýtka!</p>
+        <p className="obyc">Myslíme si, že každý by se měl někdy uvolnit z pracovního stresu a vyhodit si z kopýtka.
+							Znáš to, když se násada na sprše promění v mikrofon? Dojdi na Majáles, zahoď všechny 
+							zábrany a ukaž všem své pravé já. Abys věděl, že je normální žít život naplno, připravili jsme si rozhovory s několika Zlíňáky.</p>
       </div>
     );
   }
