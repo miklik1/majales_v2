28753f627a36b3e899d9b42ed6b64385b052d4d9
miklik99 <40503150+miklik99@users.noreply.github.com>
2019-04-01 18:22:42 +0200

    d

--- src/Doprogram.js
+++ src/Doprogram.js
@@ -23,20 +23,60 @@ class Home extends Component {
         <div className="d-flex bd-highlight razeni">
           <div className="p-2 flex-fill bd-highlight sekce1">
             <p>Souboj kapel</p>
-            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
-              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+            <span className="popis">Věčná majálesová sláva, roční zásoba piva Starobrno
+             a možnost zahrát si před zlínskou veřejností - to čeká vítěze souboje kapel.
+            </span>
+            <span className="poslech">
+              PŘIHLÁSIT
+            </span>
+          </div>
+          <div className="p-2 flex-fill bd-highlight sekce2">
+            <p>Kino</p>
+            <span className="popis">Pustit si kvalitní kultovku doma na videu
+             je vždycky záruka kvalitního večera, ale zajít si na klasiku do kina? To ti zprostředkujeme v rámci majálesového kina jenom my.
             </span>
             <span className="poslech">
               POSLECH
             </span>
           </div>
-          <div className="p-2 flex-fill bd-highlight sekce2">
-            <p>Kino</p>
-            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
-              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+        </div>
+        <div className="d-flex bd-highlight razeni">
+          <div className="p-2 flex-fill bd-highlight sekce3">
+            <p>SILENT DISCO</p>
+            <span className="popis">Takovou párty Zlín ještě neslyšel, aneb nejtišší kalba na střeše jednadvacítky. 
+            To by Baťa koukal! Pro více info sleduj fb událost.
             </span>
             <span className="poslech">
-              POSLECH
+              UDÁLOST
+            </span>
+          </div>
+          <div className="p-2 flex-fill bd-highlight sekce4">
+            <p>WARM UP PARTY</p>
+            <span className="popis">Tato zahřívačka pro všechny nedočkavce, kterým už skáčou kopýtka před Majálesem. 
+            </span>
+            <span className="poslech">
+              UDÁLOST
+            </span>
+          </div>
+        </div>
+        <div className="d-flex bd-highlight razeni">
+          <div className="p-2 flex-fill bd-highlight sekce5">
+            <p>ČISTÝ FESTIVAL</p>
+            <span className="popis">Čistý festival zajišťuje od roku 2008 třídění odpadů na českých
+             hudebních festivalech. Najdi barevné koše nebo náš stánek, třiď odpady a získej recyklované odměny.
+            </span>
+            <span className="poslech">
+              INFORMACE
+            </span>
+          </div>
+          <div className="p-2 flex-fill bd-highlight sekce6">
+            <p>KRÁL MAJÁLESU</p>
+            <span className="popis">Chceš být králem nebo královnou celého Majálesu
+             a hrdě si nasadit vítěznou korunu? Probojuj se nelehkou a trnitou cestou
+              až k majálesovému trůnu a vyhraj skvělé ceny. Přihlašuj se v odkazu.
+            </span>
+            <span className="poslech">
+              PŘIHLÁSIT
             </span>
           </div>
         </div>
--- src/Lineup.js
+++ src/Lineup.js
@@ -5,6 +5,8 @@ import './Lineup.sass';
 import Intro2 from './Components/Intro/Intro2';
 import demo from './demo.png'
 import udg from './udg.png'
+import lvcas from './lvcas.png'
+import malalata from './malalata.png'
 
 class Home extends Component {
   render() {
@@ -17,16 +19,120 @@ class Home extends Component {
             <p className="kopyto">Stage</p>
             <p className="cas">14.45-15.00</p>
             <p className="interpret">Qočna</p>
+            <p className="cas">15.00-16.00</p>
+            <p className="interpret">Like it</p>
+            <p className="cas">18.00-19.00</p>
+            <p className="interpret">Cirkus Problem</p>
+            <p className="cas">20.00-21.00</p>
+            <p className="interpret">Kapitán Demo</p>
+            <p className="cas">22.00-23.00</p>
+            <p className="interpret">Lvcas Dope</p>
+            <p className="cas">23.00-0.00</p>
+            <p className="interpret">Malalata</p>
           </div>
           <div className="p-2 flex-fill bd-highlight sekce2 h">
             <p className="kopyto">Captain Morgan</p>
             <p className="kopyto">Stage</p>
             <p className="cas">16.00-17.00</p>
             <p className="interpret">Franz Alpa</p>
+            <p className="cas">17.00-18.00</p>
+            <p className="interpret">Glad For Today</p>
+            <p className="cas">19.00-20.00</p>
+            <p className="interpret">John Wolfhooker</p>
+            <p className="cas">21.00-22.00</p>
+            <p className="interpret">UDG</p>
           </div>
         </div>
         <div className="seznam">
         <div className="d-flex bd-highlight razeni">
+          <div className="p-2 flex-fill bd-highlight sekce1">
+            <img className='demo' src={demo} alt='demo' />
+            <p>Kapitán Demo</p>
+            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
+              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+            </span>
+            <span className="poslech">
+              POSLECH
+            </span>
+          </div>
+          <div className="p-2 flex-fill bd-highlight sekce2">
+            <p>UDG</p>
+            <img className='udg' src={udg} alt='udg' />
+            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
+              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+            </span>
+            <span className="poslech">
+              POSLECH
+            </span>
+          </div>
+        </div>
+        <div className="d-flex bd-highlight razeni">
+          <div className="p-2 flex-fill bd-highlight sekce3">
+            <img className='lvcas' src={lvcas} alt='lvcas' />
+            <p>LVCAS  DOPE</p>
+            <span className="popis">Vyšlá hvězda české rapové scény, člen labelu Milion+, jehož vliv podmanil tisíce fans, 
+            nejen díky jeho spolupráci s Pauliem Garandem, DJ Wichem či Rytmusem. Jeho totálně dope show uslyšíme i na našem Majálesu.
+            </span>
+            <span className="poslech">
+              POSLECH
+            </span>
+          </div>
+          <div className="p-2 flex-fill bd-highlight sekce4">
+            <p>MALALATA</p>
+            <img className='malalata' src={malalata} alt='malalata' />
+            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
+              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+            </span>
+            <span className="poslech">
+              POSLECH
+            </span>
+          </div>
+        </div>
+                <div className="d-flex bd-highlight razeni">
+          <div className="p-2 flex-fill bd-highlight sekce1">
+            <img className='demo' src={demo} alt='demo' />
+            <p>Kapitán Demo</p>
+            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
+              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+            </span>
+            <span className="poslech">
+              POSLECH
+            </span>
+          </div>
+          <div className="p-2 flex-fill bd-highlight sekce2">
+            <p>UDG</p>
+            <img className='udg' src={udg} alt='udg' />
+            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
+              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+            </span>
+            <span className="poslech">
+              POSLECH
+            </span>
+          </div>
+        </div>
+                <div className="d-flex bd-highlight razeni">
+          <div className="p-2 flex-fill bd-highlight sekce1">
+            <img className='demo' src={demo} alt='demo' />
+            <p>Kapitán Demo</p>
+            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
+              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+            </span>
+            <span className="poslech">
+              POSLECH
+            </span>
+          </div>
+          <div className="p-2 flex-fill bd-highlight sekce2">
+            <p>UDG</p>
+            <img className='udg' src={udg} alt='udg' />
+            <span className="popis">Nejtlustší česká celebrita, vizionář a mecenáš přijede se svou Demo Crew oznámit, že je Mládí v trapu. 
+              Alternativní rap z jiné dimenze, vlna absurdna, ale v neposlední řadě humor, který ti prodlouží život, když se s ním “svezeš”!
+            </span>
+            <span className="poslech">
+              POSLECH
+            </span>
+          </div>
+        </div>
+                <div className="d-flex bd-highlight razeni">
           <div className="p-2 flex-fill bd-highlight sekce1">
             <img className='demo' src={demo} alt='demo' />
             <p>Kapitán Demo</p>
